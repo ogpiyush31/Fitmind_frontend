c72de45dfb11c2f3c93f0067f31b63dc66857c18
ogpiyush31 <piyushcs8340@gmail.com>
2025-05-23 21:19:21 +0530

    add url of render

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://fitmind-backend.onrender.com'
 };
 
 export default CONFIG;
